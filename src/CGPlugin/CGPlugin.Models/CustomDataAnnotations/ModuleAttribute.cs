6ac62b2963c521c559cc9e7620e6a841bfbf5947
SukharevMA <maxs130872@gmail.com>
2022-11-15 18:29:49 +0700

    Переработка валидации модели

---
+++ src/CGPlugin/CGPlugin.Models/CustomDataAnnotations/ModuleAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace CGPlugin.Models.CustomDataAnnotations;
+
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+///   Настройка валидации значения модуля шестерни
+/// </summary>
+public class ModuleAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is null)
+            return new ValidationResult(ErrorMessage);
+
+        var gearModel = (CitroenGearModel) validationContext.ObjectInstance;
+
+        var uintVal = (uint) value;
+        var module = gearModel.Diameter / (gearModel.TeethCount + 2);
+
+        if (uintVal == 0 || uintVal != module)
+            return new ValidationResult(ErrorMessage);
+
+        return ValidationResult.Success;
+    }
+}
