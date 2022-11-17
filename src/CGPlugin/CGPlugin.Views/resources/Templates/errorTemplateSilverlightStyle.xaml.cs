b62b1610d13a82e0b2deed0de3230cd99ca7d4d9
SukharevMA <maxs130872@gmail.com>
2022-11-17 22:41:59 +0700

    Изменение стиля валидации TextBox + Переаботка ресурсов
    Подключение библиотеки Microsoft.Xaml.Behaviors.Wpf
    Добавление словаря ресурсов для приложения

---
+++ src/CGPlugin/CGPlugin.Views/resources/Templates/errorTemplateSilverlightStyle.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CGPlugin.Views.Resources.Templates;
+
+using System.Windows.Controls;
+
+/// <summary>
+///   Логика взаимодействия для errorTemplateSilverlightStyle.xaml
+/// </summary>
+public class ErrorTemplateSilverlightStyle : ControlTemplate
+{
+    
+}
